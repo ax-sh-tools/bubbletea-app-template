ff546670c69c570f3a9af7af2674fb8395726977
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2025-03-23 15:42:09 +0545

    refactor: update model to include stopwatch functionality

--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"github.com/charmbracelet/bubbles/stopwatch"
 	"os"
+	"time"
 
 	"github.com/charmbracelet/bubbles/key"
 	"github.com/charmbracelet/bubbles/spinner"
@@ -13,25 +15,26 @@ import (
 type errMsg error
 
 type model struct {
-	spinner  spinner.Model
-	quitting bool
-	err      error
+	spinner   spinner.Model
+	stopwatch stopwatch.Model
+	quitting  bool
+	err       error
 }
 
 var quitKeys = key.NewBinding(
 	key.WithKeys("q", "esc", "ctrl+c"),
-	key.WithHelp("", "press q to quit"),
+	key.WithHelp("", "Press q to quit"),
 )
 
 func initialModel() model {
 	s := spinner.New()
 	s.Spinner = spinner.Dot
 	s.Style = lipgloss.NewStyle().Foreground(lipgloss.Color("205"))
-	return model{spinner: s}
+	return model{spinner: s, stopwatch: stopwatch.NewWithInterval(time.Millisecond)}
 }
 
 func (m model) Init() tea.Cmd {
-	return m.spinner.Tick
+	return tea.Batch(m.spinner.Tick, m.stopwatch.Start())
 }
 
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
@@ -49,9 +52,11 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		return m, nil
 
 	default:
-		var cmd tea.Cmd
-		m.spinner, cmd = m.spinner.Update(msg)
-		return m, cmd
+		var spinnerCmd tea.Cmd
+		var stopwatchCmd tea.Cmd
+		m.spinner, spinnerCmd = m.spinner.Update(msg)
+		m.stopwatch, stopwatchCmd = m.stopwatch.Update(msg)
+		return m, tea.Batch(spinnerCmd, stopwatchCmd)
 	}
 }
 
@@ -59,17 +64,24 @@ func (m model) View() string {
 	if m.err != nil {
 		return m.err.Error()
 	}
-	str := fmt.Sprintf("\n\n   %s Loading forever... %s\n\n", m.spinner.View(), quitKeys.Help().Desc)
+	str := lipgloss.JoinVertical(lipgloss.Left,
+		m.spinner.View()+"Loading forever...",
+		quitKeys.Help().Desc,
+		"Elapsed Time: "+m.stopwatch.View(),
+	)
+	//str := fmt.Sprintf("\n\n   %s Loading forever... %s\n\n", m.spinner.View(), quitKeys.Help().Desc)
 	if m.quitting {
-		return str + "\n"
+		return str
 	}
 	return str
 }
 
 func main() {
-	p := tea.NewProgram(initialModel())
+	m := initialModel()
+	p := tea.NewProgram(m)
 	if _, err := p.Run(); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
+	println(m.quitting)
 }
